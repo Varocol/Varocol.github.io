354fd5d29fb59bfcdeb38e9468d1ea48c009cb34
Varocol <1628553830@qq.com>
2021-11-11 16:48:33 +0800

    Site updated: 2021-11-11 16:48:31

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Varocol/ImgHosting/Varocol_Blog/Fluid_img/avatar.png");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Varocol/ImgHosting/Varocol_Blog/Fluid_img/favicon.png");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
---
+++ js/cnzz.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1280441904'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1280441904%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
